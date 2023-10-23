42caeb594c205123b1058b50c79ce9a81d709830
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2023-10-22 22:40:36 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -3,13 +3,13 @@ Author: Christopher Pederson 2023-10-20
 Description: JavaScript for the slider page
 Co-Author:
 this is a random content generator that will cycle through provided content and display it in a random order
-if you add to the project please place your name and the date in the co author section above
+if you add to the project, please place your name and the date in the co-author section above
 */
 
 const thumbnailsArray = []; // this array holds your image paths in a string format
-const namesArray = []; // this array hold the slides title name in a string format
-const descriptionsArray = []; // this array holds the slides description in a string format
-let slideArray = [];// this array holds the slide objects but you will not need to add content to this array
+const namesArray = []; // this array holds the slides title name in a string format
+const descriptionsArray = []; // this array holds the description of the slide in a string format
+let slideArray = [];//This array holds the slide objects, but you will not need to add content to this array
 
 class slideObj {
     constructor(thumbnail, name, description) {
@@ -19,7 +19,7 @@ class slideObj {
     }
 }
 
-// this event listener is to make sure the DOM is loaded before the script is run
+//This event listener is to make sure the DOM is loaded before the script is run
 document.addEventListener('DOMContentLoaded', function () {
     const slideCount = (document.querySelectorAll('[data-slide-id]')).length;
 
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     setSlides(slideArray, slideCount);
 });
 
-// the purpose of this function is to place the info from all three image, name, and description arrays into one _array of objects
+//The purpose of this function is to place the info from all three image, name, and description arrays into one _array of objects
 function setArray(_slideArray, _thumbnailsArray, _namesArray, _descriptionsArray) {
     for (let i = 0; i < _thumbnailsArray.length; i++) {
         _slideArray.push(new slideObj(_thumbnailsArray[i], _namesArray[i], _descriptionsArray[i]));
@@ -36,7 +36,7 @@ function setArray(_slideArray, _thumbnailsArray, _namesArray, _descriptionsArray
     console.log(_slideArray);
 }
 
-// the purpose of this function is to shuffle the array to randomize the occurance of slides
+//The purpose of this function is to shuffle the array to randomize the occurrence of slides
 function shuffleArray(_array) {
     let currentIndex = _array.length, temporaryValue, randomIndex;
 
@@ -53,7 +53,7 @@ function shuffleArray(_array) {
     }
 }
 
-// the purpose of this function is to set the slides in the DOM
+//The purpose of this function is to set the slides in the DOM
 function setSlides(_array, index){
     const imageElements = document.querySelectorAll('[data-image-id]');
     const nameElements = document.querySelectorAll('[data-name-id]');
